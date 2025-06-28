118c145df0a8cb5dab13e51c1204e40e28a8cd0a
ahmetbuyukballi <ahmetbuyukballi@gmail.com>
2025-06-28 17:17:58 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example", exclusive: false);
+
+//Queueya mesaj gönderme işlemi
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul eder.Mesajları byte türünden göndermek lazım.
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example", body: message);
+
+Console.ReadLine();
