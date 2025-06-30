b5eda5df694b97132f58e77a26d5d77767f37e77
ahmetbuyukballi <ahmetbuyukballi@gmail.com>
2025-06-30 15:57:34 +0300

    GelismisKuyrukMimarisi

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -13,12 +13,22 @@ using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
 //Queue oluşturma
-channel.QueueDeclare(queue: "example", exclusive: false);
+channel.QueueDeclare(queue: "example", exclusive: false,durable:true);
 
 //Queueya mesaj gönderme işlemi
 //RabbitMQ kuyruğa atacağı mesajları byte türünden kabul eder.Mesajları byte türünden göndermek lazım.
 
-byte[] message = Encoding.UTF8.GetBytes("Merhaba");
-channel.BasicPublish(exchange: "", routingKey: "example", body: message);
+
+//Mesajların kalıcılığı için aşağıdaki yöntemi kullanacağız
+
+IBasicProperties basicProperties=channel.CreateBasicProperties();
+basicProperties.Persistent = true;
+for(int i=0;i<100;i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba"+ i);
+    channel.BasicPublish(exchange: "", routingKey: "example", body: message,basicProperties:basicProperties);
+}
+
 
 Console.ReadLine();
